54a084e37efc7406693704aac9f91d490ffa1bd5
honeycuttr9 <56522490+honeycuttr9@users.noreply.github.com>
2020-07-05 13:14:20 -0400

    Update StockTracker.py

--- StockTracker.py
+++ StockTracker.py
@@ -1,11 +1,13 @@
-
 # Gets today's stock price at the most recent close, for each stock you enter.
 # Stock price is appended to a file and user receives a text of current stock prices
-# Additional functionality is still being built and added :) 
+# Additional functionality is still being built and added :)
 
 #! /usr/bin/python3
 
 # import libraries
+import StockList as sl
+import sys
+from termcolor import colored
 import requests, time, urllib
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -14,57 +16,72 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from twilio.rest import Client
 
-stocks = []
-
-while True:
-    ticker_sym = input("Enter ticker symbol or company name ")
-    if not ticker_sym:
-        break
-    else:
-        stocks.append(ticker_sym)
-
 #display all stocks to find prices of
-print("Please find the price of the following stocks below", stocks)
+def output(stock_list):
+    print("Please find the price of the following stocks below", stock_list)
 
+    #setup webdriver and navigate to Yahoo Finance
+    service = Service(r"C:\path\to\chromedriver.exe")
+    service.start()
+    driver = webdriver.Remote(service.service_url)
+    driver.get('https://finance.yahoo.com/')
 
-#setup webdriver and navigate to Yahoo Finance
-service = Service(r"C:\Users\rhone\Downloads\chromedriver_win32\chromedriver.exe")
-service.start()
-driver = webdriver.Remote(service.service_url)
-driver.get('https://finance.yahoo.com/')
-
-#Find stock info
-for stock in stocks:
-    #get info from yahoo finance using ticker_sym
-    html = driver.execute_script('return document.body.innerHTML')
-    text_area = driver.find_element_by_id('yfin-usr-qry')
-    text_area.send_keys(stock)
-    time.sleep(5)
-    driver.find_element_by_id('header-desktop-search-button').click()
-    time.sleep(1)
-    url = driver.current_url
-    res = requests.get(url)
-    soup = BeautifulSoup(res.content, 'lxml')
-    price = soup.select_one('.Trsdu\(0\.3s\)').text
-    output = stock + " price at close is " + price
-    print(output)
-driver.quit()
+    #Find stock info
+    for stock in stock_list:
+        #get info from yahoo finance using ticker_sym
+        html = driver.execute_script('return document.body.innerHTML')
+        text_area = driver.find_element_by_id('yfin-usr-qry')
+        text_area.send_keys(stock)
+        time.sleep(5)
+        driver.find_element_by_id('header-desktop-search-button').click()
+        time.sleep(1)
+        url = driver.current_url
+        res = requests.get(url)
+        soup = BeautifulSoup(res.content, 'lxml')
+        price = soup.select_one('.Trsdu\(0\.3s\)').text
+        output = stock + " price at close is " + price
+        print(output)
+    driver.quit()
 
 # append to file
 
 #send text message
-#change variables to reflect yours for twilio and your cell phone number
-account_sid = '<Enter account SID from twilio>'
-auth_token = '<Enter authentication token from twilio>'
-client = Client(account_sid, auth_token)
+#change variables to reflect yours for twilio and your cell phone number \_(ツ)_/
 
-message = client.messages \
-    .create(
-        body = output,
-        from_ = '+1<# provided from twilio>',
-        to = '+1<cell # to send message to>'
-)
+def send_txt(from_num, to_num):
+    account_sid = '<Enter your twilio account sid>'
+    auth_token = '<Enter your twilio auth token>'
+    client = Client(account_sid, auth_token)
+    
+    message = client.messages \
+        .create(
+            body = output,
+            from_ = '+<Enter your twilio #>',
+            to = '+<Enter cell # to send text to>'
+    )
+    
+    print(message.sid)
 
-print(message.sid)
 
+def main():
 
+    stock_list = []
+
+    while True:
+        stock = input("Enter ticker symbol or company name (Enter q or quit to get stock info)")
+        if stock.lower() == 'q' or stock.lower() == 'quit':
+            break
+        if (sl.does_exist(r'C:\Users\rhone\Desktop\company_names.txt', stock)) or (sl.does_exist(r'C:\Users\rhone\Desktop\ticker_sym.txt', stock)):
+            stock_list.append(stock)
+            print(colored(stock, 'green'), "has been added to list of stocks")
+        else:
+            print(colored(stock, 'red'), "cannot be found in the NASDAQ, NYSE or AMEX")
+
+    if len(stock_list) == 0:
+        print("No stocks provided")
+        sys.exit(0)
+    else:
+        output(stock_list)
+
+if __name__ == '__main__':
+    main()
